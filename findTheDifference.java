788e04536f5d7d145d549f1e767e210aa2703efb
mamathaguntu <mamathaguntu@gmail.com>
2020-09-24 11:36:41 -0700

    findTheDifference.java

---
+++ findTheDifference.java
@@ -0,0 +1,25 @@
+package FindDifference;
+
+// import java.util.HashSet;
+
+public class findTheDifference {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+        char ch = new findTheDifference().findDifference(s, t);
+        System.out.println(ch);
+    }
+    public char findDifference(String s, String t) {
+        char ch = ' ';
+        char[] c = t.toCharArray();
+        for (int i = 0; i < c.length; i++)
+        {
+            if (s.indexOf(c[i]) < 0)
+            {
+                ch = c[i];
+                break;
+            }
+        }
+        return ch;
+    }
+}
